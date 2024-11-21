1300055c56a8f1a5879bc460ddd09ffd520c089d
Anuj <anujparasar4266@gmail.com>
2024-11-21 23:51:14 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,36 @@
+$(".menu > ul > li").click(function (e) {
+  // remove active from already active
+  $(this).siblings().removeClass("active");
+  // add active to clicked
+  $(this).toggleClass("active");
+  // if has sub menu open it
+  $(this).find("ul").slideToggle();
+  // close other sub menu if any open
+  $(this).siblings().find("ul").slideUp();
+  // remove active class of sub menu items
+  $(this).siblings().find("ul").find("li").removeClass("active");
+});
+
+$(".menu-btn").click(function () {
+  $(".sidebar").toggleClass("active");
+});
+
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
